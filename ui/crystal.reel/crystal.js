d76df26c72ead6ee2701d8518960feac83980a6d
nitrousracer <noreply>
2017-03-19 07:26:43 +0000

    Add component crystal

---
+++ ui/crystal.reel/crystal.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/crystal.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Crystal
+ * @extends Component
+ */
+exports.Crystal = Component.specialize(/** @lends Crystal# */ {
+    constructor: {
+        value: function Crystal() {
+            this.super();
+        }
+    }
+});
